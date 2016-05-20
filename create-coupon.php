901d364e1f4d22b282130bc0626f861019f3a714
Thoriq Firdaus <tfirdaus@outlook.com>
2016-05-20 19:44:08 +0700

    add func. for unqiue coupon ID and plugin header

--- create-coupon.php
+++ create-coupon.php
@@ -1,10 +1,37 @@
 <?php 
+/**
+ * Plugin Name: WooCommerce Auto-Coupon
+ * Plugin URI: https://github.com/hongkiat/woocommerce-auto-coupon
+ * Description: Create only when customer sees the WooCommerce "thank you" page.
+ * Version: 0.0.1
+ * Author: Marton Fekete
+ * Author URI: https://github.com/martonfekete
+ * Requires at least: 4.1
+ * Tested up to: 4.5
+ *
+ * Text Domain: woo-auto-coupon
+ * Domain Path: /i18n/languages/
+ *
+ * @package WooCommerce
+ * @category Core
+ * @author WooThemes
+ */
+
 /*
  * CREATE COUPON PROGRAMATICALLY 
  * create only when customer sees the woocommerce thankyou page
  * create only if there is no coupon with the same code
  */
- 
+function generateRandomString($length = 5) {
+    $characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
+
 add_action( 'woocommerce_thankyou', 'my_custom_coupon_creation');
 
 function my_custom_coupon_creation(){
@@ -28,9 +55,10 @@ function my_custom_coupon_creation(){
 	foreach ( $coupons as $coupon ) {
 		$coupon_name = $coupon->post_title;
 		array_push( $coupon_names, $coupon_name );
-		}	
+	}	
 	
-	$coupon_code = 'YOUR_COUPON';	// your coupon code
+	// $coupon_code = 'YOUR_COUPON';	// your coupon code
+	$coupon_code = generateRandomString();
 	
 	// check if there's already a coupon with that code
 	if ( !in_array( $coupon_code , $coupon_names  ) ){
@@ -48,11 +76,8 @@ function my_custom_coupon_creation(){
 		$new_coupon_id = wp_insert_post( $coupon ); // create the coupon and fetch it's ID
 		
 		// Set some coupon options here
-	 
 		update_post_meta( $new_coupon_id, 'coupon_amount', '10' );        // coupon will be for 10%
 		update_post_meta( $new_coupon_id, 'discount_type', 'percent' );   // coupon will be for 10%
-	
-	}
-	
+	}	
 }
 ?>
